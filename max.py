bd2e86f2c5a88b0dbd53a9efd107abba1f8fdcfe
emiemilyu <156430367+emiemilyu@users.noreply.github.com>
2024-02-14 17:56:11 +0000

    I comitted this file

---
+++ max.py
@@ -0,0 +1,11 @@
+a=5*6*8
+b=3*4*2
+p=5*2*9
+if a>b or a>p:
+    print('a>b or a>p,min vab=',a) 
+elif a<b or a<p:
+    print('b>a or p>a,min value=',b)
+else:
+    print('a==b or a==p,value=',a)
+
+
