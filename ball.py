25773f5b0459367400ec4b091e28604b931d2ae9
emiemilyu <156430367+emiemilyu@users.noreply.github.com>
2024-03-19 13:55:23 +0000

    I comitted this file

---
+++ ball.py
@@ -0,0 +1,8 @@
+word='ball'
+new_word=''
+for x in word:
+    new_word=x + new_word
+    if word == new_word:
+      print('palindrome')
+
+    
---
+++ door.py
@@ -0,0 +1,7 @@
+word='door'
+symbol='o'
+counter='0'
+for x in word:
+    if word==symbol:
+        counter + 1 
+        print('word')
